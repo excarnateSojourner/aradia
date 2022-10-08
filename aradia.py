cf3fca3c7a34d8bbac2f258c013ed70fe467b846
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2022-10-07 23:14:49 -0700

    Let live and script paths be arbitrary dirs.
    Let --live-path and --scripts-path be paths of arbitrary directories
    rather than the live path having to be a sibling of aradia.py, and the
    scripts path a child of the live path.
    Also factor out the making of these directories if they do not yet
    exist.

--- aradia.py
+++ aradia.py
@@ -19,8 +19,7 @@ from response import Response
 GET_EXCEPTIONS = {'favicon.ico': Response(HTTPStatus.GONE)}
 POST_EXCEPTIONS = {}
 DEFAULT_HEADERS = ['user-agent', 'referer', 'content-type', 'content-length']
-CWD = os.path.dirname(os.path.realpath(__file__))
-UP = os.path.pardir + os.path.sep
+SERVER_ONLY_ARGS = ['address', 'port']
 
 def main():
 	parser = argparse.ArgumentParser()
@@ -34,21 +33,22 @@ def main():
 	parser.add_argument('-r', '--log-request-len', default=200, type=int, help='The length to which requests should be truncated when logging them.')
 	args = parser.parse_args()
 
-	os.chdir(CWD + os.path.sep + args.live_path)
-	if not os.path.exists(args.scripts_path):
-		os.mkdir(args.scripts_path)
-	full_time_path = UP + args.last_post_time_path
-	if not os.path.exists(full_time_path):
-		with open(full_time_path, 'w') as time_file:
+	make_dirs_safe(args.scripts_path)
+	make_dirs_safe(os.path.dirname(args.last_post_time_path))
+	try:
+		with open(args.last_post_time_path, 'x') as time_file:
 			print('0', file=time_file)
-	request_handler_args = {k: v for k, v in vars(args).items() if k not in ['address', 'port']}
+	except FileExistsError:
+		pass
+
+	request_handler_args = {k: v for k, v in vars(args).items() if k not in SERVER_ONLY_ARGS}
 	with http.server.HTTPServer((args.address, args.port), functools.partial(AradiaRequestHandler, **request_handler_args)) as httpd:
-		print(f'Serving {args.live_path}{os.path.sep} at {args.address}:{args.port}.')
+		print(f'Serving {os.path.realpath(os.path.curdir)} at {args.address}:{args.port}.')
 		try:
 			httpd.serve_forever()
 		except KeyboardInterrupt:
 			print('\nExiting.')
-			raise SystemExit(0)
+			exit(0)
 
 class AradiaRequestHandler(http.server.SimpleHTTPRequestHandler):
 	def __init__(self, request, client_address, server, live_path='live', scripts_path='scripts', log_path='aradia.log', last_post_time_path='last_post_time.int', log_headers=DEFAULT_HEADERS, log_request_len=200):
@@ -56,58 +56,67 @@ class AradiaRequestHandler(http.server.SimpleHTTPRequestHandler):
 		self.scripts_path = scripts_path
 		self.log_path = log_path
 		self.last_post_time_path = last_post_time_path
+		self.original_dir = os.getcwd()
 		super().__init__(request, client_address, server)
 
 	def do_GET(self):
-		# Remove initial slash.
-		path = self.path[1:]
-		if path in GET_EXCEPTIONS:
+		if self.path[1:] in GET_EXCEPTIONS:
 			self.send(GET_EXCEPTIONS[path])
-		# If a Python program meant to produce requests was requested, reject.
-		elif os.path.exists(path) and os.path.samefile(os.path.dirname(path), self.scripts_path):
+			return
+
+		path = os.path.join(self.live_path, self.path[1:])
+		full_scripts_path = os.path.realpath(self.scripts_path)
+		# If a Python script meant to produce requests was requested, reject. (This check is necessary if scripts_path is a subpath of live_path.)
+		if os.path.commonpath([full_scripts_path, os.path.realpath(path)]) == full_scripts_path:
 			self.send_error(HTTPStatus.METHOD_NOT_ALLOWED)
 		# If a directory with no index.html was requested, reject.
-		elif (os.path.isdir(path) or not path) and not os.path.isfile(os.path.join(path, 'index.html')):
+		elif os.path.isdir(path) and not os.path.isfile(os.path.join(path, 'index.html')):
 			self.send_error(HTTPStatus.FORBIDDEN, explain='This server does not give directory listings')
 		# If the '.html' has been omitted, add it automatically and redirect.
 		elif not os.path.exists(path) and not os.path.splitext(path)[1] and os.path.isfile(path + '.html'):
 			self.send(Response(HTTPStatus.MOVED_PERMANENTLY, {'Location': path + '.html'}))
 		else:
+			self.directory = os.path.join(self.directory, self.live_path)
 			super().do_GET()
+			self.directory = self.original_dir
 
 	def do_POST(self):
 		'''
-		Runs Python programs in self.scripts_path.
-		The parameters in the request body are parsed into a dict of lists and saved as self.parameters. This entire AradiaRequestHandler is then passed to the program's main() function. (This gives the program access to things like the client_address, headers.get('referer'), and the last_post_time in addition to the parameters sent in the request.) If the program is modifies any part of this handler (including by calling one of its non-static methods), undefined behaviour will occur.
-		The Python program is expected to return a Response (as defined in response.py).
+		Runs Python scripts in self.scripts_path.
+		The parameters in the request body are parsed into a dict of lists and saved as self.parameters. This entire AradiaRequestHandler is then passed to the script's main() function. (This gives the script access to things like the client_address, headers.get('referer'), and the last_post_time in addition to the parameters sent in the request.) If the script is modifies any part of this handler (including by calling one of its non-static methods), undefined behaviour will occur.
+		The Python script is expected to return a Response (as defined in response.py).
 		'''
 
-		# Remove initial slash.
-		path = self.path[1:]
-		if path in POST_EXCEPTIONS:
+		if self.path[1:] in POST_EXCEPTIONS:
 			self.send(POST_EXCEPTIONS[path])
-		path_match = re.fullmatch(f'({self.scripts_path}{os.path.sep}[a-zA-Z0-9_\\-{os.path.sep}]+)\\.py', path)
-		with open(UP + self.last_post_time_path) as last_post_time_file:
-			last_post_time = int(last_post_time_file.read()[:-1])
+			return
+		# Be extra careful to avoid malicious input.
+		path_match = re.fullmatch(f'([a-zA-Z0-9\\-_{os.path.sep}]+)\\.py', self.path[1:])
+		with open(self.last_post_time_path) as last_post_time_file:
+			last_post_time = int(last_post_time_file.read().rstrip('\n'))
+		path = os.path.join(self.scripts_path, self.path[1:])
 		if int(time.time()) - last_post_time < 15:
 			self.send_error(HTTPStatus.TOO_MANY_REQUESTS, explain='You must wait a minimum of 15 seconds between POST requests. Use your browser\'s back button and try again after waiting')
 		elif not path_match:
-			self.send_error(HTTPStatus.METHOD_NOT_ALLOWED)
+			self.send_error(HTTPStatus.FORBIDDEN, explain=f'The paths of scripts may only contain alphanumeric ASCII characters, "{os.path.sep}", and ".py" at the end')
+		elif os.path.isdir(path):
+			self.send_error(HTTPStatus.FORBIDDEN, explain='This server does not give directory listings')
 		elif not os.path.isfile(path):
 			self.send_error(HTTPStatus.NOT_FOUND)
 		# Good request.
 		else:
-			with open(UP + self.last_post_time_path, 'w') as last_post_time_file:
-				last_post_time_file.write(str(int(time.time())) + '\n')
-			# Even though the working directory is self.live_path, importlib.import_module imports relative to the program that it is called in.
-			program = f'{self.live_path}{os.path.sep}{path_match[1]}'.replace(os.path.sep, '.')
+			with open(self.last_post_time_path, 'w') as last_post_time_file:
+				print(int(time.time()), file=last_post_time_file)
 			request_body = self.rfile.read(int(self.headers.get('content-length'))).decode()
 			self.parameters = urllib.parse.parse_qs(request_body, keep_blank_values=True)
-			module = importlib.import_module(program)
+			# Dynamically import using just the path.
+			spec = importlib.util.spec_from_file_location(os.path.splitext(os.path.basename(path))[0], path)
+			module = importlib.util.module_from_spec(spec)
+			spec.loader.exec_module(module)
 			try:
 				response = module.main(self)
 			except:
-				response = Response(500, body='The program called to handle the request raised an exception.')
+				response = Response(HTTPStatus.INTERNAL_SERVER_ERROR, body='The script called to handle the request raised an exception.')
 				self.log_message(traceback.format_exc(limit=10))
 			self.send(response)
 
@@ -146,8 +155,17 @@ class AradiaRequestHandler(http.server.SimpleHTTPRequestHandler):
 				message = message.format(args)
 			except TypeError:
 				pass
-		with open(UP + self.log_path, 'a') as log_file:
+		with open(self.log_path, 'a') as log_file:
 			log_file.write(f'{message}\n{"#" * 20}\n')
 
+def make_dirs_safe(path):
+	if os.path.exists(path):
+		if not os.path.isdir(path):
+			print(f'Error: "{path}" exists, but it is not a directory.', file=sys.stderr)
+			exit(1)
+	else:
+		os.makedirs(os.path.realpath(path), exist_ok=True)
+
 if __name__ == '__main__':
+
 	main()
